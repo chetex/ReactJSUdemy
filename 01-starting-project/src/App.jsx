43139b8289cf195bacc83b000abbdd15bbb6c8dc
chetex <chetex69@gmail.com>
2024-10-22 11:58:21 +0200

    New dynamic variable added

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,16 +1,38 @@
+/**
+ * Array with elements to show
+ */
+const codeHeaderWordsArray = ['Fundamental', 'Core', 'Important'];
+
+/**
+ * Generate random numbers
+ * @param {*} max 
+ * @returns 
+ */
+function generateCodeHeaderWords (max) {
+  return Math.floor(Math.random() * max);
+}
+
+/**
+ * Component header with JSX code
+ * @returns JSX html code
+ */
 function Header() {
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Nacho</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {codeHeaderWordsArray[generateCodeHeaderWords(3)]} react concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
   );
 }
 
+/**
+ * Main function APP given on enter
+ * @returns 
+ */
 function App() {
   return (  
     <div> 
