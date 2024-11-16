85d17b90dbae84934fe5bc671aa44e64c2af3ded
chetex <chetex69@gmail.com>
2024-11-16 16:05:05 +0100

    handleClick and events

--- 06-children-props-and-links/src/components/TapButton.jsx
+++ 06-children-props-and-links/src/components/TapButton.jsx
@@ -1,4 +1,12 @@
 // deno-lint-ignore-file
-export default function TabButton (props) {
-    return <li><button>{props.children}</button></li>;
+export default function TabButton ({ children }) {
+    function handleClick () {
+        console.log("Hello click");
+    }
+
+    return ( 
+        <li>
+            <button onClick={handleClick}>{children}</button>
+        </li>
+    );
 }
