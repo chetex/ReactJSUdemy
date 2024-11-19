ebfbb13bd7bf747dad0730ec9a252d64225db46b
chetex <chetex69@gmail.com>
2024-11-19 21:56:09 +0100

    First hook useState interaction over component

---
+++ 08-managing-state-hooks/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+
+import { CORE_CONCEPTS } from './data.js';
+import Header from './components/Header/Header.jsx';
+import Avatar from './components/Avatar.jsx';
+import CoreConcepts from './components/CoreConcepts.jsx';
+import TabButton from './components/TapButton.jsx';
+
+/**
+ * Main function APP given on enter
+ * @returns 
+ */
+function App() {
+  const [tabButton, setTabButtonName] = useState("components");
+
+  function handleSelect (tabButtonName) {
+    setTabButtonName(tabButtonName);
+    console.log ("selected => " + tabButtonName);
+  } 
+
+  return (  
+    <div> 
+      <Header/>
+      <main>
+        <h2>Time to get started!</h2>
+        <section>
+          <ul>
+            <Avatar {...CORE_CONCEPTS[0]}/>
+            <Avatar {...CORE_CONCEPTS[1]}/>
+            <CoreConcepts {...CORE_CONCEPTS[2]}/>
+            <CoreConcepts {...CORE_CONCEPTS[3]}/>
+          </ul>
+        </section>
+      </main>
+      <section id="examples">
+        <h2>Examples</h2>
+        <menu>
+          <TabButton onSelect={() => handleSelect("components")}>Components</TabButton>
+          <TabButton onSelect={() => handleSelect("jsx")}>Jsx</TabButton>
+          <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+          <TabButton onSelect={() => handleSelect("stats")}>State</TabButton>
+        </menu>
+        {tabButton}
+      </section>
+    </div>
+  );
+}
+
+export default App;
